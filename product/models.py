150ce332569fabe2b31967823924f3d81c4eecfa
kunal <kunalbhatia868@gmail.com>
2021-12-01 18:46:06 +0530

    Product Model Added

--- product/models.py
+++ product/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    
+    class StatusChoices(models.TextChoices):
+        AVAILABLE="SA","Stock Available"
+        OUTOFSTOCK="NSA","No Stock Available"
+
+    title=models.CharField(max_length=256)
+    slug=models.SlugField(blank=True,null=True)
+    description=models.TextField()
+    image=models.ImageField()
+    status=models.CharField(max_length=3,choices=StatusChoices.choices,default=StatusChoices.AVAILABLE)
+    published_date=models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title[:50]
+    
